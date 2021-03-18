25137ea9aa9be02a8aad205ad9e88b0e83d74671
rache930 <ryutaro.093031@gmail.com>
2021-03-18 10:53:46 +0900

    ログイン、ログアウト機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
+    validates :password, on: :create,  format: {with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
     validates :nickname#, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."}
     validates :last_name#, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."}
     validates :first_name#, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."}
