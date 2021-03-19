6fa6b728f36569f8c1942216219551adeee4346f
rache930 <ryutaro.093031@gmail.com>
2021-03-19 12:23:52 +0900

    ActiveHashを導入する

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_date
   belongs_to :user
   has_one_attached :image
 
-  
 end
