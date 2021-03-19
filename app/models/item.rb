e5bb8ad6829c56e55cd837a523c65605c5dec07f
rache930 <ryutaro.093031@gmail.com>
2021-03-19 17:24:45 +0900

    テーブルへの保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,18 +9,17 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   half_num = /\A[0-9]+\z/
-  jp_yen = greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999
 
   with_options presence: true do
     validates :name
     validates :text
     with_options  numericality: { other_than: 1 } do
-      validates :category_id, 
+      validates :category_id
       validates :condition_id
       validates :shipping_fee_id
-      validates :prefecture_id
       validates :shipping_date_id
     end
-    validates :price, format: {with: half_num }, numericality: {jp_yen}
+    validates :prefecture_id, numericality: { other_than: 0}
+    validates :price, format: {with: half_num }, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   end
 end
