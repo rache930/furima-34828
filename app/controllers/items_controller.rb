83a7f1b851031dd094cc576a9c9bd7a735d9d621
rache930 <ryutaro.093031@gmail.com>
2021-03-20 18:16:02 +0900

    rubocop修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create ]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
   end
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :text, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_date_id,
-                                 :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :text, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
+                                 :shipping_date_id, :price).merge(user_id: current_user.id)
   end
 end
