44f19f43a932322ac4a307d4b313826f65c90001
rache930 <ryutaro.093031@gmail.com>
2021-03-22 23:05:32 +0900

    購入機能に必要なモデルやコントローラーを作成する

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,21 @@
+class OrdersController < ApplicationController
+  def index
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end  
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: item_id)
+  end  
+end
