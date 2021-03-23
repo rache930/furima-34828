6d99bc13ad0178997f7b911c2ad084a8bf4597a2
rache930 <ryutaro.093031@gmail.com>
2021-03-23 16:55:55 +0900

    エラーが表示されるようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :item_find, only: [:index, :create]
   def index
     @order_address = OrderAddress.new
   end
@@ -9,13 +10,17 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end  
 
   private
+  
+  def item_find
+    @item = Item.find(params[:item_id])
+  end
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: item_id)
+    params.require(:order_address).permit(:postal_code, :prefecture, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end  
 end
