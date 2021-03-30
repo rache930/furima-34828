7642cf21cb1840c2f1ac939cfbbe96fa21c3c2a3
rache930 <ryutaro.093031@gmail.com>
2021-03-30 23:39:18 +0900

    ユーザー名変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.16.0'
 set :application, 'furima-34828'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:rache/furima-34828.git'
+set :repo_url,  'git@github.com:rache930/furima-34828.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
