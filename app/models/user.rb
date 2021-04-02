fff5b42fe27aedbb19bd886bfecf9ce5cfa18e41
rache930 <ryutaro.093031@gmail.com>
2021-04-03 00:36:08 +0900

    SNSアカウントを使ったログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+  has_many :sns_credentials
   has_many :items
   has_many :orders
 
   valid_password = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i
 
   with_options presence: true do
-    validates :password, on: :create, format: { with: valid_password, message: 'is invalid. Input half-width characters.' }
+    validates :password, on: :create, format: { with: valid_password, message: 'is invalid. Input half-width characters.' }, unless: -> { validation_context == :hoge }
     validates :nickname
     validates :birth_date
     with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' } do
@@ -22,4 +22,20 @@ class User < ApplicationRecord
       validates :first_name_reading
     end
   end
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save!(context: :hoge)
+    end
+    { user: user, sns: sns }
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_093454) do
+ActiveRecord::Schema.define(version: 2021_04_02_100354) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -79,6 +79,15 @@ ActiveRecord::Schema.define(version: 2021_03_31_093454) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "tag_name", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -110,4 +119,5 @@ ActiveRecord::Schema.define(version: 2021_03_31_093454) do
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
+  add_foreign_key "sns_credentials", "users"
 end
